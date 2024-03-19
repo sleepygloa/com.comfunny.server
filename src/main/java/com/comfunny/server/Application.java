e57db3a08ae46611e141214adbf8752a82575aff
seonhokim <seonhokim@seonhokimui-MacBookPro.local>
2024-03-20 00:20:49 +0900

    add Application

---
+++ src/main/java/com/comfunny/server/Application.java
@@ -0,0 +1,18 @@
+package com.comfunny.server;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+@Slf4j
+public class Application extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        SpringApplicationBuilder springApplicationBuilder = new SpringApplicationBuilder(Application.class);
+        SpringApplication springApplication = springApplicationBuilder.build();
+        springApplication.run(args);
+    }
+}
